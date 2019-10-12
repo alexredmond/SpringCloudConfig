73ad459404b170a6d1055327a35e86d117136d1a
unknown <kiva@o2.pl>
2019-10-12 16:20:57 +0100

    3

---
+++ Config_client/src/main/java/iva/config/client/RateController.java
@@ -0,0 +1,14 @@
+package iva.config.client;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RateController {
+	
+	String rate;
+	String lanecount;
+	String toll;
+	
+
+}
+
---
+++ Config_client/src/main/java/iva/config/client/SpringCloudConficCLientApplication.java
@@ -0,0 +1,13 @@
+package iva.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConficCLientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConficCLientApplication.class, args);
+	}
+
+}
